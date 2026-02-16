1748fc64c9d0c887c7bad6e5131ed5ce35bbf07e
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2026-02-16 22:55:00 +0300

    feat(mobile-nav): close dropdown on Escape key and outside click

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ let menu = document.querySelector('.menu-icon');
 let sliderMenu = document.querySelector('.slider-menu');
 
 function toggleMenu() {
-    const isOpen = sliderMenu.classList.contains("active");
-
     sliderMenu.classList.toggle("active");
-    menu.setAttribute("aria-expanded", !isOpen);
+    const isOpen = sliderMenu.classList.contains("active");
+    menu.setAttribute("aria-expanded", isOpen);
 }
+
+
+function closeMenu() {
+    sliderMenu.classList.remove("active");
+     menu.setAttribute("aria-expanded", "false");
+}
+
+// close when esc key is pressed
+document.addEventListener("keydown" ,function(event){
+ if(event.key === "Escape"){
+    closeMenu();
+ }
+});
+
+// close when clicking outside
+document.addEventListener("click", function(event) {
+    const isClickInsideMenu = sliderMenu.contains(event.target);
+    const isClickOnButton = menu.contains(event.target);
+
+    if(!isClickInsideMenu && !isClickOnButton) {
+       closeMenu();
+    }
+});
 
 menu.addEventListener("click", toggleMenu);
